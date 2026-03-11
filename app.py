14206701581f6052634b9bb2a862dc7e3597e231
Chris MacBook <macbook@chris-macbook.home>
2026-03-11 18:53:49 +0100

    Initialisation du site web

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-# je vais tout faire ici vous inquietez pas 
-# c'est pour lancer l'app web
+# je vais tout faire ici vous inquietez pas (c'est pour lancer l'app web)
+
+from flask import Flask
+from routes import routes_bp
+
+app = Flask(__name__)
+app.register_blueprint(routes_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,11 @@
+# ici on va faire toutes les routes et c'est ici que vous devriez coder. 
+# Tous ce qui est déjà présent et que vous ne comprenais pas c'est que c'est pour flask
+# Ne toucher à rien je vous expliquerai tout.
+
+from flask import Blueprint, render_template
+
+routes_bp = Blueprint('routes', __name__)
+
+@routes_bp.route('/')
+def welcome():
+    return render_template('index.html')
