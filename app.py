4860c316f4b35c305e373fe33c5fdf986564d751
Chris MacBook <macbook@Chris-MacBook.local>
2026-05-15 17:38:58 +0200

    adaptation pour vps

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 
 from flask import Flask, session
 from routes import routes_bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(routes_bp)
-app.secret_key = 'tu_ne_trouvera_jamais_cette_cle'
+app.secret_key = os.getenv('SECRET_KEY', 'tu_ne_trouvera_jamais_cette_cle')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    port = int(os.environ.get("PORT", 5002))
+    app.run(debug=True, port=port)
